fbc7c1ecde8a4a3dfe49493be7e3642496eb177e
Yves Bissaya <yves.botele@nomadia.com>
2025-08-24 12:01:47 +0200

    create deamon cli

--- cloud_containers.sh
+++ cloud_containers.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 action=$1
-projet_path="/home/yves/nextcloud"
+current_path=$(pwd)
 
 case "$action" in
     "start" )
         echo "start all cloud container"
         docker network create nextcloud
-        cd $projet_path/docker &&  docker compose -f docker-compose.yml build && docker compose -f docker-compose.yml up -d
-        cd $projet_path/common/portainer &&  docker compose -f docker-compose.yml build && docker compose -f docker-compose.yml up -d
+        cd $current_path/docker &&  docker compose -f docker-compose.yml build && docker compose -f docker-compose.yml up -d
+        cd $current_path/common/portainer &&  docker compose -f docker-compose.yml build && docker compose -f docker-compose.yml up -d
         docker ps
         ;;
     "stop" )
         echo "Stop all cloud container"
-        cd $projet_path/docker && docker compose -f docker-compose.yml down
-        cd $projet_path/common/portainer && docker compose -f docker-compose.yml down
+        cd $current_path/docker && docker compose -f docker-compose.yml down
+        cd $current_path/common/portainer && docker compose -f docker-compose.yml down
         docker ps
         ;;
     * )
-        echo "No action"
+        echo "Usage : /etc/init.d/cloud_containers.sh start|stop"
         ;;
 esac
